203ab98cc6d567d9be0c06686900546f97037d6e
nonshalant <shamir.gibbons@gmail.com>
2023-11-30 16:59:26 -0500

    finished designing the home component

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -2,13 +2,14 @@ import React from 'react'
 import './home.css'
 import image from '../../IMG_1345.pdf'
 import { Image } from 'cloudinary-react'
+import { FaCamera } from 'react-icons/fa'
 
 const Home = () => {
   return (
     <div className='home'>
         <div className='home-content'>
-            <h1>an aspiring author + editor ✏</h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem doloribus dolores ratione temporibus officiis, recusandae aspernatur rem obcaecati eligendi, quaerat placeat cum iste? Obcaecati facere, hic quam animi quidem praesentium!</p>
+            <h1>an aspiring creator <FaCamera /></h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem doloribus dolores ratione temporibus officiis, recusandae aspernatur rem obcaecati eligendi</p>
         </div>
         <div className="home-image">
             <Image cloudName='dyzydm9nl' publicId='https://res.cloudinary.com/dyzydm9nl/image/upload/v1701374706/Janelle%20Portfolio/IMG_1345_mxtxlx.jpg'/>
