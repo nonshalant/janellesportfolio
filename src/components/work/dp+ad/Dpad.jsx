d40f63480de37239e126a87582f0487d0b331125
nonshalant <shamir.gibbons@gmail.com>
2023-12-02 00:56:24 -0500

    testing design on mobile added embeded video

--- src/components/work/dp+ad/Dpad.jsx
+++ src/components/work/dp+ad/Dpad.jsx
@@ -20,6 +20,9 @@ const Dpad = () => {
             )
           }
         </div>
+        <div className="video">
+          <iframe width="560" height="315" src="https://www.youtube.com/embed/0kPZbJxlwik?si=gq-erC1iqFw6iLP0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+        </div>
       </div>
     </div>
   )
